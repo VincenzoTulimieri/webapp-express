1908d35dd88562faf0d767ff6d975e83f2463c41
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-05-09 16:46:49 +0200

    feat: create rotte index e show

---
+++ routers/moviesRouter.js
@@ -0,0 +1,17 @@
+// express e router
+const express = require('express');
+const router= express.Router()
+
+// importazione controller
+
+const moviesController = require('../controllers/moviesController')
+
+// index router
+router.get('/',moviesController.index);
+
+// show router
+router.get('/:id',moviesController.show);
+
+
+// esportazione router
+module.exports = router
