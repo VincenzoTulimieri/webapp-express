432ce0e30dab8952d2bb56a12e53d9ce53893a24
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-05-09 17:07:07 +0200

    feat: creato file connessione con DB

---
+++ data/db.js
@@ -0,0 +1,18 @@
+// importazione mysql
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'vincenzo22',
+    database:'movies_db'
+})
+
+connection.connect((err)=>{
+    if(err){
+        throw err
+    }
+    console.log('Connesso a MYsql, ci sono anche io')
+})
+
+module.exports = connection
