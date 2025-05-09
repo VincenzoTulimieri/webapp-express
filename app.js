77f092ac173f2710d5825d0e20ff418f5724afdf
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-05-09 16:25:27 +0200

    feat: creati middlewares

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
+// importazione express con porta 
 const express = require('express')
 const app = express()
 const port = 3000
 
+// importazione middlewares
+const notFound = require('./middlewares/notFound')
+const errorServer = require('./middlewares/errorsHandlers')
+
+// file disponibili al client
+app.use(express.static('public'))
+
+// uso middlewares
+app.use(notFound)
+app.use(errorServer)
+
+// server in attesa
 app.listen(port,()=>{
     console.log(`Resto in ascolto sulla porta ${port}`)
 })
