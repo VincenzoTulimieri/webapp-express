84d205fc1899c00220e55b36ab0884e3ea1f30c9
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-05-12 14:46:58 +0200

    fix: formattazione file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // importazione express con porta 
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000 
+const port = process.env.PORT || 3000
 
 // importazione router
 const moviesRouter = require('./routers/moviesRouter')
@@ -17,8 +17,8 @@ app.use(express.static('public'))
 app.use(express.json())
 
 // pagina di benvenuto
-app.get('/', (req,res)=>{
-    res.send('Ecoo la mia web app')
+app.get('/', (req, res) => {
+    res.send('Ecco la mia web app')
 })
 
 // utilizzo router
